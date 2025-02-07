207d9b6cabdff07e5182f11dd82e10af0e01b1b8
OmPrakashMadasi <koushiknaidu2@gmail.com>
2025-02-07 11:21:55 +0530

    404 error pages

--- orisonproject/urls.py
+++ orisonproject/urls.py
@@ -6,6 +6,12 @@ from django.conf.urls.static import static
 from webapp import views
 from django.contrib.auth import views as auth_views
 
+from django.conf.urls import handler404, handler500
+
+handler404 = 'webapp.views.custom_404'
+handler500 = 'webapp.views.custom_500'
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('webapp.urls')),
--- webapp/views.py
+++ webapp/views.py
@@ -368,3 +368,10 @@ def checkout(request, slug):
         return redirect("home")  # Redirect to homepage after order is placed
 
     return redirect("home", slug=slug)
+
+
+def custom_404(request, exception):
+    return render(request, '404.html', status=404)
+
+def custom_500(request):
+    return render(request, '500.html', status=500)
